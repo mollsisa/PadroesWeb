181de7715af6dc83e3e84188a5818eb08373114c
AndreHenriqueCaseiro <andrehenriquecaseiro@gmail.com>
2025-06-10 18:15:34 -0300

    html/page2.html

--- js/pages/secondPage.js
+++ js/pages/secondPage.js
@@ -9,6 +9,129 @@ const dicasArray = [
   'Dica 3: .'
 ];
 
+// Seletor Data de Nascimento
+const seletorDia = document.getElementById('seletor-dia');
+const seletorMes = document.getElementById('seletor-mes');
+const seletorAno = document.getElementById('seletor-ano');
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+let contadorDia = 1;
+let contadorDiaFlag = 1;
+let contadorDiaIntervalo;
+let contadorDiaIntervaloTempo;
+let contadorDiaDefinido = 1;
+let contadorMes = 0;
+let contadorMesDefinido = 1;
+let contadorAno = 0;
+let contadorAnoDefinido = 1;
+let dataInformada;
+const hoje = new Date();
+
+//Função para mudar o dia
+function mudarDia(){
+  if(contadorDiaFlag == 1){
+    contadorDia++;
+    if(contadorDia >= 31){
+      contadorDiaFlag = 0;
+    }
+  }
+  else{
+    contadorDia--;
+    if(contadorDia == 1){
+      contadorDiaFlag = 1;
+    }
+  }
+  contadorDiaIntervaloTempo = Math.floor(Math.random() * 300) + 1;
+  seletorDia.textContent = contadorDia;
+};
+//Função mudar o mês
+function qualMes(){
+  contadorMes = Math.floor(Math.random() * 12);
+}
+//Função limitar o Ano
+function limitarAno(){
+   if (contadorAno >= 2025){
+    contadorAno = 2025
+    contadorAnoDefinido=0;
+  }
+}
+//Função resetar campos das datas
+function resetarDatas(){
+  contadorDiaDefinido = 1;
+  contadorMesDefinido = 1;
+  contadorAnoDefinido = 1;
+  contadorAno = 0;
+  clearInterval(contadorDiaIntervalo);
+  seletorDia.textContent = 'Dia';
+  seletorMes.textContent = 'Mês';
+  seletorAno.textContent = 'Ano';
+}
+
+//Controle da data de nascimento
+seletorDia.addEventListener('mouseenter', () =>{
+  if (contadorDiaDefinido){
+    clearInterval(contadorDiaIntervalo);
+    contadorDiaIntervalo = setInterval(mudarDia,contadorDiaIntervaloTempo)
+  }
+});
+seletorDia.addEventListener('mouseleave', () =>{
+  if (contadorDiaDefinido){
+    clearInterval(contadorDiaIntervalo);
+    contadorDiaIntervalo = setInterval(mudarDia,contadorDiaIntervaloTempo)
+  }
+});
+seletorDia.addEventListener('click', () =>{
+   if (contadorDiaDefinido){
+  contadorDia = Math.floor(Math.random() * 31) + 1;
+  }
+});
+seletorMes.addEventListener('click', () =>{
+  clearInterval(contadorDiaIntervalo);
+  contadorDiaDefinido = 0;
+});
+seletorMes.addEventListener('dblclick', () =>{
+  if(contadorMesDefinido){
+  qualMes();
+  seletorMes.textContent = meses[contadorMes];
+  }
+});
+seletorAno.addEventListener('click',() =>{
+  contadorMesDefinido = 0;
+  if(contadorAnoDefinido){
+  contadorAno++;
+  }
+  limitarAno;
+  seletorAno.textContent = contadorAno;
+});
+seletorAno.addEventListener('contextmenu',() =>{
+  contadorMesDefinido = 0;
+  if(contadorAnoDefinido){
+  contadorAno = contadorAno + 975;
+  }
+  limitarAno();
+  seletorAno.textContent = contadorAno;
+});
+
+//Resetar Data
+const botaoErrei = document.getElementById('editar');
+botaoErrei.addEventListener('dblclick', () =>{
+  resetarDatas();
+});
+
+//Enviar
+const botaoAcertei = document.getElementById('salvar');
+botaoAcertei.addEventListener('dblclick', () =>{
+ if(
+  contadorAno == hoje.getFullYear &&
+  contadorMes == hoje.getMonth &&
+  contadorDia == hoje.getDay
+ ){
+  mudarDia();
+ }
+ else{
+  botaoAcertei.textContent = hoje.getFullYear;
+ }
+});
+
 let contador = 0;
 
 btn.addEventListener('click', () => {
@@ -36,8 +159,6 @@ inputs.forEach(input => {
   });
 
 });
-
-
 
     const btnVoltar = document.getElementById('btnVoltar');
     const btnProximo = document.getElementById('btnProximo');
